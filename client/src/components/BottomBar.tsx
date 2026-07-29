f102ad864c1fee71d6fd036f9ebd9c01b32ae527
fanta <fantacasino3012@gmail.com>
2026-07-29 23:40:13 +0200

    feat(ui): mise a jour version 2.0 sur l'accueil et configuration bots hardcore

--- client/src/components/BottomBar.tsx
+++ client/src/components/BottomBar.tsx
@@ -6,7 +6,7 @@ import { navigate } from '../router.js';
 export default function BottomBar() {
   return (
     <footer className="bottom-bar">
-      <span>Version 1.1</span>
+      <span>Version 2.0</span>
       <span>Angul.io 2026</span>
       <button
         type="button"
